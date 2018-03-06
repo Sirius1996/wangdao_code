ac26c3b02b287ece9ea567daa502cc1381534aee
Sirius1996 <976952479@qq.com>
2018-03-06 21:27:31 +0800

    problem4

---
+++ problem4.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+    stack<int> S;
+    char str[110];
+    char ans[110];
+
+int main()
+{
+
+    while((scanf("%s",str))!='\0')
+    {
+        int i;
+        for(i=0;str[i]!=0;i++)
+        {
+            if(str[i]=='(')
+            {
+                S.push(i);
+                ans[i]=' ';
+            }
+            else if(str[i]==')')
+            {
+                if(S.empty()==false)
+                {
+                    S.pop();
+                    ans[i]=' ';
+                }
+                else
+                    ans[i]='?';
+            }
+            else
+                ans[i]=' ';
+        }
+        while(!S.empty())
+        {
+            ans[S.top()]='$';
+            S.pop();
+        }
+    ans[i]=0;
+    puts(str);
+    puts(ans);
+    }
+    return 0;
+
+}
