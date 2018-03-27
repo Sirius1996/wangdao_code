9a315bceef922cbdf7fc63e11b3361f56ecc12b8
Sirius1996 <976952479@qq.com>
2018-03-27 16:03:39 +0800

    北邮2014网研ProblemA

---
+++ BUPT/problem6.cpp
@@ -0,0 +1,35 @@
+//北邮网研2014 ProblemA
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n>0)
+    {
+        int a,b,temp,above=1,below=1;
+        cin>>a>>b;
+        if(a>b)
+        {
+            temp=a;
+            a=b;
+            b=temp;
+        }
+        while(a!=b)
+        {
+            above++;
+            a++;
+        }
+        above++;
+        while(a>0)
+        {
+            below=below*2;
+            a--;
+        }
+        cout<<above<<"/"<<below<<endl;
+        n--;
+    }
+    return 0;
+}
